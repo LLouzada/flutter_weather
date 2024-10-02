effc9ec7227eb0bd0874f9dbab2d8f7fc39cd0da
LLouzada <llouzada@usp.br>
2024-10-01 21:55:10 -0300

    Finalizado Map Container

--- lib/presentation/pages/weather_page.dart
+++ lib/presentation/pages/weather_page.dart
@@ -4,7 +4,8 @@ import 'package:flutter_weather/app/config/app_routes.dart';
 import 'package:flutter_weather/presentation/controllers/theme_controller.dart';
 import 'package:flutter_weather/presentation/controllers/weather_controller.dart';
 import 'package:flutter_weather/presentation/pages/weather_partials/forecast_container.dart';
-import 'package:flutter_weather/presentation/pages/weather_partials/today_weather.dart';
+import 'package:flutter_weather/presentation/pages/weather_partials/map_container.dart';
+import 'package:flutter_weather/presentation/pages/weather_partials/today_weather_conteiner.dart';
 import 'package:get/get.dart';
 
 class WeatherPage extends StatelessWidget {
@@ -20,55 +21,7 @@ class WeatherPage extends StatelessWidget {
         title: const Text(AppConstants.appName),
         centerTitle: true,
         actions: [
-          PopupMenuButton<int>(
-            icon: const Icon(Icons.menu),
-            onSelected: (value) {
-              switch (value) {
-                case 0:
-                  themeController.toggleTheme();
-                  break;
-                case 1:
-                  Get.toNamed(AppRoutes.search.path);
-                  break;
-                case 2:
-
-                // Adicione mais cases se houver mais opções
-              }
-            },
-            itemBuilder: (context) => [
-              const PopupMenuItem(
-                value: 0,
-                child: Row(
-                  children: [
-                    Icon(Icons.brightness_6),
-                    SizedBox(width: 8),
-                    Text('Trocar tema'),
-                  ],
-                ),
-              ),
-              const PopupMenuItem(
-                value: 1,
-                child: Row(
-                  children: [
-                    Icon(Icons.location_pin),
-                    SizedBox(width: 8),
-                    Text('Procurar Cidade'),
-                  ],
-                ),
-              ),
-              const PopupMenuItem(
-                value: 2,
-                child: Row(
-                  children: [
-                    Icon(Icons.settings),
-                    SizedBox(width: 8),
-                    Text('Configurações'),
-                  ],
-                ),
-              ),
-              // Você pode adicionar mais opções aqui
-            ],
-          ),
+          _MainPopupMenuButton(themeController: themeController),
         ],
       ),
       body: SingleChildScrollView(
@@ -80,23 +33,76 @@ class WeatherPage extends StatelessWidget {
             ForecastWeatherContainer(
               weatherController: weatherController,
             ),
-            Container(
-              height: 200,
-              margin: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Center(
-                child: Text(
-                  'Mapa de Localização',
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                ),
-              ),
-            ),
+            MapContainer(weatherController: weatherController),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _MainPopupMenuButton extends StatelessWidget {
+  final ThemeController themeController;
+
+  const _MainPopupMenuButton({
+    required this.themeController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        PopupMenuButton<int>(
+          icon: const Icon(Icons.menu, size: 40),
+          onSelected: (value) {
+            switch (value) {
+              case 0:
+                themeController.toggleTheme();
+                break;
+              case 1:
+                Get.toNamed(AppRoutes.search.path);
+                break;
+              case 2:
+
+              //todo - mais opções
+            }
+          },
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              value: 0,
+              child: Row(
+                children: [
+                  Icon(Icons.brightness_6),
+                  SizedBox(width: 8),
+                  Text('Trocar tema'),
+                ],
+              ),
+            ),
+            const PopupMenuItem(
+              value: 1,
+              child: Row(
+                children: [
+                  Icon(Icons.location_pin),
+                  SizedBox(width: 8),
+                  Text('Procurar Cidade'),
+                ],
+              ),
+            ),
+            const PopupMenuItem(
+              value: 2,
+              child: Row(
+                children: [
+                  Icon(Icons.settings),
+                  SizedBox(width: 8),
+                  Text('Configurações'),
+                ],
+              ),
+            ),
+            // Você pode adicionar mais opções aqui
+          ],
+        ),
+        const SizedBox(width: 16),
+      ],
     );
   }
 }
---
+++ lib/presentation/pages/weather_partials/map_container.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_weather/presentation/controllers/weather_controller.dart';
+import 'package:get/get.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapContainer extends StatelessWidget {
+  const MapContainer({
+    super.key,
+    required this.weatherController,
+  });
+
+  final WeatherController weatherController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      height: 600,
+      margin: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: MapBuilder(weatherController: weatherController),
+      ),
+    );
+  }
+}
+
+class MapBuilder extends StatelessWidget {
+  final WeatherController weatherController;
+
+  const MapBuilder({super.key, required this.weatherController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      if (weatherController.isWeatherLoading.value &&
+          weatherController.isCityLoading.value) {
+        return const Center(
+            child: CircularProgressIndicator()); // ou outra UI de loading
+      }
+
+      var location = weatherController.locationModel.value;
+      var currentLatLng = LatLng(location!.latitude, location.longitude);
+
+      return FlutterMap(
+        options: MapOptions(initialCenter: currentLatLng),
+        children: [
+          TileLayer(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: currentLatLng,
+                child: Icon(Icons.location_pin,
+                    color: Theme.of(Get.context!).colorScheme.error, size: 40),
+              ),
+            ],
+          ),
+        ],
+      );
+    });
+  }
+}
